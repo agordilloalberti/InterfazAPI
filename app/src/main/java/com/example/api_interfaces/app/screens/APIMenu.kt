03dd19dfd18ae59f44ffe93fe38b3ec88d9d9fb1
Alvarogogal <agorsua0610@g.educaand.es>
2025-03-06 16:43:16 +0100

    Added some interface changes

--- app/src/main/java/com/example/api_interfaces/app/screens/APIMenu.kt
+++ app/src/main/java/com/example/api_interfaces/app/screens/APIMenu.kt
@@ -1,13 +1,18 @@
 package com.example.api_interfaces.app.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.api_interfaces.app.AddButton
@@ -28,14 +33,22 @@ fun APIMenu(navControlador: NavHostController, modifier: Modifier, viewModel: My
 }
 
 @Composable
-private fun Body(navControlador: NavController, modifier: Modifier,viewModel: MyViewModel){
-    Column(modifier = modifier
-        .background(Color.Black)
-        .fillMaxSize())
-    {
-        AddButton("Login") {navControlador.navigate(AppScreen.APIUser.route);viewModel.changeScreen("login")}
-        AddButton("Registrar usuario") {navControlador.navigate(AppScreen.APIUser.route);viewModel.changeScreen("register")}
-        AddButton("Gestionar tareas") {navControlador.navigate(AppScreen.APITareas.route)}
-        AddButton("Volver") {navControlador.navigate(AppScreen.MainScreen.route)}
+private fun Body(navControlador: NavController, modifier: Modifier, viewModel: MyViewModel) {
+    Column(
+        modifier = modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            AddButton("Login") { navControlador.navigate(AppScreen.APIUser.route); viewModel.changeScreen("login") }
+            AddButton("Registrar usuario") { navControlador.navigate(AppScreen.APIUser.route); viewModel.changeScreen("register") }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            AddButton("Gestionar tareas") { navControlador.navigate(AppScreen.APITareas.route) }
+            AddButton("Volver") { navControlador.navigate(AppScreen.MainScreen.route) }
+        }
     }
 }
